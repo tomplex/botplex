0fe9ace3a3aa212a59c8f6fcf42eccca01784785
carusot <carusot42@gmail.com>
2015-05-26 23:46:11 -0400

    added log and Setlist class for parsing Json

--- bot.py
+++ bot.py
@@ -1,62 +1,39 @@
 __author__ = 'carusot'
 
 import time
-import json
-
 import praw
-import requests
-
-from set_parse import full_output, checker
 
 
-checkList = r'/Users/carusot/Documents/Python/setlistBot/aq/checked'
-#checkList = r'/home/tomplex/botplex/checked'
+from aq.tools.setlist import Setlist
 
-# loads list of comments that have been replied to already
-with open(checkList, 'r') as f:
-    checked = json.load(f)
+from aq.tools.bot_tools import reply,log,checker,find_date
 
 
 # bot itself
-bot = praw.Reddit('Aqueous setlist fetcher, v1.1 by /u/eyesoftheworld4')
-bot.login('botplex','warren')
+bot = praw.Reddit('Aqueous setlist fetcher, v1.2 by /u/eyesoftheworld4')
+bot.login('botplex', 'warren')
 
-# forever:
 while True:
-    # get comments for
+
     sub = bot.get_subreddit('aqueousband')
-    cmts = sub.get_comments()
 
-    for comment in cmts:
-        body = comment.body
-        if 'AQ' in body and comment.id not in checked:
-            checked.append(comment.id)
-            with open(checkList, 'w') as f:
-                json.dump(checked, f)
+    for comment in sub.get_comments():
 
-            if checker(body) is None:
-                continue
-
-            r = requests.get('http://aqueousband.com/shows/{}.json'.format(checker(body)))
-
-            if r.status_code == 200:
-                comment.reply(full_output(r.json()))
-                print('replied with setlist')
-                time.sleep(300)
+        if checker(comment.body, comment.id) is False:
+            continue
 
 
-            else:
-                comment.reply('Sorry, setlist not found. Check to see if you have [the right date.]'
-                              '(http://www.aqueousband.com/shows)')
-                print('replied with setlist not found')
-                time.sleep(300)
+        set = Setlist(find_date(comment.body))
 
+        if set.compiled is None:
+            reply('Sorry, setlist not found!', comment, 1)
 
         else:
-            pass
-    x = time.localtime()
-    print(str(x[0])+'-'+str(x[1])+'-'+str(x[2])+', '+str(x[3])+'hrs '+str(x[4])+'mins '+str(x[5])+'sec'+
-          ':sleeping for 10 minutes')
+            reply(set.compiled, comment, 0)
+
+
+
+    log('waiting 10 minutes to re-check comments')
     time.sleep(600)
 
 
