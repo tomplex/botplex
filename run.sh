c818e93edcd8e381729f6e2021f51ad310431b79
Tom Caruso <carusot42@gmail.com>
2018-09-04 01:36:10 -0400

    Change run script to use -d

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t botplex .
-docker run -v $PWD/log:/botplex/log --rm --name botplex_prod -p 8001:8000 botplex
+docker run -d -v $PWD/log:/botplex/log --rm --name botplex_prod -p 8001:8000 botplex
